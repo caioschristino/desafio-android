df20eb188df959977bd19fabcb33ed9c7af7f7e1
Caio Sanchez <caioschristino@gmail.com>
2018-04-18 18:55:16 -0300

    refatorar interface api parar kotlin

--- app/src/main/java/com/v2/desafionubank/api/NuMobileApi.kt
+++ app/src/main/java/com/v2/desafionubank/api/NuMobileApi.kt
@@ -1,5 +1,29 @@
 package com.v2.desafionubank.api
 
+import com.v2.desafionubank.model.*
+import io.reactivex.Observable
+import retrofit2.http.Body
+import retrofit2.http.GET
+import retrofit2.http.POST
+import retrofit2.http.Url
+
 /**
  * Created by CaioSChristino on 18/04/18.
  */
+
+interface NuMobileApi {
+    @GET
+    fun GetBodyRequestFromUrl(@Url url: String): Observable<ResponseLink>
+
+    @GET
+    fun GetNoticeFromUrl(@Url url: String): Observable<ResponseNotice>
+
+    @GET
+    fun GetChargebackFromUrl(@Url url: String): Observable<ResponseChargeback>
+
+    @POST
+    fun BlockUnblockCard(@Url url: String): Observable<ResponsePost>
+
+    @POST
+    fun SendContest(@Url url: String, @Body requestLockCard: RequestLockCard): Observable<ResponsePost>
+}
