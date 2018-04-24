cdf5364051f3b939bc8679a929bbe2fd28af694d
csanchez <csanchez@brq.com>
2018-04-24 13:03:15 -0300

    refatorar classes modelos faltantes

---
+++ app/src/main/java/com/v2/desafionubank/model/ResponseError.kt
@@ -0,0 +1,7 @@
+package com.v2.desafionubank.model
+
+/**
+ * Created by csanchez on 24/04/2018.
+ */
+
+class ResponseError(val error: String)
---
+++ app/src/main/java/com/v2/desafionubank/model/ResponseLink.kt
@@ -0,0 +1,9 @@
+package com.v2.desafionubank.model
+
+/**
+ * Created by csanchez on 24/04/2018.
+ */
+
+class ResponseLink {
+    val links: Links? = null
+}
